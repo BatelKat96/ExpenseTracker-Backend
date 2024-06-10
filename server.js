416e8a42e399dd2074deb29345a18d344760797f
BatelKat96 <batelkat@gmail.com>
2024-06-10 15:23:32 +0300

    Create logger

--- server.js
+++ server.js
@@ -7,7 +7,8 @@ import { fileURLToPath } from 'url'
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
 
-
+import { logger } from './services/logger.service.js'
+logger.info('server.js loaded...')
 
 const app = express()
 
