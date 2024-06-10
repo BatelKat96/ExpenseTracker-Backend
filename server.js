ed31a07279c9e17b9245ac70257cb61e13dddf31
BatelKat96 <batelkat@gmail.com>
2024-06-10 15:18:13 +0300

    Initial project

---
+++ server.js
@@ -0,0 +1,47 @@
+import express from 'express'
+import cookieParser from 'cookie-parser'
+import cors from 'cors'
+import path, { dirname } from 'path'
+import { fileURLToPath } from 'url'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+
+
+
+const app = express()
+
+// Express App Config
+app.use(cookieParser())
+app.use(express.json())
+app.use(express.static('public'))
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.resolve(__dirname, 'public')))
+    console.log('__dirname: ', __dirname)
+} else {
+    const corsOptions = {
+        origin: [
+            'http://127.0.0.1:5173',
+            'http://localhost:5173',
+            'http://127.0.0.1:3000',
+            'http://localhost:3000',
+            'http://127.0.0.1:8080',
+            'http://localhost:8080',
+        ],
+        credentials: true
+    }
+    app.use(cors(corsOptions))
+}
+
+
+
+app.get('/**', (req, res) => {
+    res.sendFile(path.resolve('public/index.html'))
+})
+
+const port = process.env.PORT || 3030
+
+app.listen(port, () => {
+    logger.info('Server is running on port: ' + port)
+})
